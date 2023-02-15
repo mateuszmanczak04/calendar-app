684f7310816fcf27571ac0d9ab4f41a2b804d9d3
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-02-15 15:03:52 +0100

    add swipe animation in week mode

--- app/week/[mondayDate]/WeekHours.tsx
+++ app/week/[mondayDate]/WeekHours.tsx
@@ -4,10 +4,14 @@ import OnlyHours from '../../(components)/OnlyHours';
 import EventsDay from '../../(components)/EventsDay';
 import { useDateContext } from '../../../context/useDateContext';
 
+const AMOUNT = 7;
+
 const WeekHours = () => {
   const { currentDate, getDateBefore, getDateAfter } = useDateContext();
   const [firstDate, setFirstDate] = useState(currentDate);
-  const [orders, setOrders] = useState([-1, 0, 1, 2, 3, 4, 5, 6, 7]);
+  const [orders, setOrders] = useState([
+    -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+  ]);
   const [transition, setTransition] = useState('0.4s ease');
 
   useEffect(() => {
@@ -22,30 +26,36 @@ const WeekHours = () => {
     }
 
     if (
-      currentDateMidnight.getTime() + 24 * 60 * 60 * 1000 !==
+      currentDateMidnight.getTime() + 7 * 24 * 60 * 60 * 1000 !==
         firstDateMidnight.getTime() &&
-      currentDateMidnight.getTime() - 24 * 60 * 60 * 1000 !==
+      currentDateMidnight.getTime() - 7 * 24 * 60 * 60 * 1000 !==
         firstDateMidnight.getTime()
     ) {
       setTransition('0s');
       setFirstDate(currentDate);
-      setOrders([-1, 0, 1]);
+      setOrders([
+        -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+        13,
+      ]);
       return;
     }
 
     setTransition('0.4s ease');
     if (firstDateMidnight > currentDateMidnight) {
       // go back
-      setOrders((prevOrders) => prevOrders.map((o) => o + 1));
+      setOrders((prevOrders) => prevOrders.map((o) => o + 7));
     } else if (firstDateMidnight < currentDateMidnight) {
       // go forward
-      setOrders((prevOrders) => prevOrders.map((o) => o - 1));
+      setOrders((prevOrders) => prevOrders.map((o) => o - 7));
     }
 
     setTimeout(() => {
       setTransition('0s');
       setFirstDate(currentDate);
-      setOrders([-1, 0, 1]);
+      setOrders([
+        -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+        13,
+      ]);
     }, 400);
   }, [currentDate, firstDate]);
 
@@ -54,57 +64,129 @@ const WeekHours = () => {
       <OnlyHours />
       <div className={styles.days}>
         <EventsDay
-          currentDate={getDateBefore(firstDate, 1)}
+          currentDate={getDateBefore(firstDate, 7)}
           order={orders[0]}
-          amount={7}
+          amount={AMOUNT}
+          transition={transition}
+        />
+        <EventsDay
+          currentDate={getDateBefore(firstDate, 6)}
+          order={orders[1]}
+          amount={AMOUNT}
+          transition={transition}
+        />
+        <EventsDay
+          currentDate={getDateBefore(firstDate, 5)}
+          order={orders[2]}
+          amount={AMOUNT}
+          transition={transition}
+        />
+        <EventsDay
+          currentDate={getDateBefore(firstDate, 4)}
+          order={orders[3]}
+          amount={AMOUNT}
+          transition={transition}
+        />
+        <EventsDay
+          currentDate={getDateBefore(firstDate, 3)}
+          order={orders[4]}
+          amount={AMOUNT}
+          transition={transition}
+        />
+        <EventsDay
+          currentDate={getDateBefore(firstDate, 2)}
+          order={orders[5]}
+          amount={AMOUNT}
+          transition={transition}
+        />
+        <EventsDay
+          currentDate={getDateBefore(firstDate, 1)}
+          order={orders[6]}
+          amount={AMOUNT}
           transition={transition}
         />
         <EventsDay
           currentDate={firstDate}
-          order={orders[1]}
-          amount={7}
+          order={orders[7]}
+          amount={AMOUNT}
           transition={transition}
         />
         <EventsDay
           currentDate={getDateAfter(firstDate, 1)}
-          order={orders[2]}
-          amount={7}
+          order={orders[8]}
+          amount={AMOUNT}
           transition={transition}
         />
         <EventsDay
           currentDate={getDateAfter(firstDate, 2)}
-          order={orders[3]}
-          amount={7}
+          order={orders[9]}
+          amount={AMOUNT}
           transition={transition}
         />
         <EventsDay
           currentDate={getDateAfter(firstDate, 3)}
-          order={orders[4]}
-          amount={7}
+          order={orders[10]}
+          amount={AMOUNT}
           transition={transition}
         />
         <EventsDay
           currentDate={getDateAfter(firstDate, 4)}
-          order={orders[5]}
-          amount={7}
+          order={orders[11]}
+          amount={AMOUNT}
           transition={transition}
         />
         <EventsDay
           currentDate={getDateAfter(firstDate, 5)}
-          order={orders[6]}
-          amount={7}
+          order={orders[12]}
+          amount={AMOUNT}
           transition={transition}
         />
         <EventsDay
           currentDate={getDateAfter(firstDate, 6)}
-          order={orders[7]}
-          amount={7}
+          order={orders[13]}
+          amount={AMOUNT}
           transition={transition}
         />
         <EventsDay
           currentDate={getDateAfter(firstDate, 7)}
-          order={orders[8]}
-          amount={7}
+          order={orders[14]}
+          amount={AMOUNT}
+          transition={transition}
+        />
+        <EventsDay
+          currentDate={getDateAfter(firstDate, 8)}
+          order={orders[15]}
+          amount={AMOUNT}
+          transition={transition}
+        />
+        <EventsDay
+          currentDate={getDateAfter(firstDate, 9)}
+          order={orders[16]}
+          amount={AMOUNT}
+          transition={transition}
+        />
+        <EventsDay
+          currentDate={getDateAfter(firstDate, 10)}
+          order={orders[17]}
+          amount={AMOUNT}
+          transition={transition}
+        />
+        <EventsDay
+          currentDate={getDateAfter(firstDate, 11)}
+          order={orders[18]}
+          amount={AMOUNT}
+          transition={transition}
+        />
+        <EventsDay
+          currentDate={getDateAfter(firstDate, 12)}
+          order={orders[19]}
+          amount={AMOUNT}
+          transition={transition}
+        />
+        <EventsDay
+          currentDate={getDateAfter(firstDate, 13)}
+          order={orders[20]}
+          amount={AMOUNT}
           transition={transition}
         />
       </div>
