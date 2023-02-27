d4a873a75832cbcf3587b323d818af0ff4bf1e2f
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-02-27 10:03:48 +0100

    recover useWindowWidth

--- hooks/useWindowWidth.ts
+++ hooks/useWindowWidth.ts
@@ -1,21 +1,21 @@
 import { useEffect, useState } from 'react';
 
 const useWindowWidth = () => {
-  // const [width, setWidth] = useState<number>(0);
+  const [width, setWidth] = useState<number>(0);
 
-  // useEffect(() => {
-  //   const resizeHandler = () => {
-  //     setWidth(window.innerWidth);
-  //   };
+  useEffect(() => {
+    const resizeHandler = () => {
+      setWidth(window.innerWidth);
+    };
 
-  //   window.addEventListener('resize', resizeHandler);
+    window.addEventListener('resize', resizeHandler);
 
-  //   return () => {
-  //     window.removeEventListener('resize', resizeHandler);
-  //   };
-  // }, []);
+    return () => {
+      window.removeEventListener('resize', resizeHandler);
+    };
+  }, []);
 
-  return 0;
+  return width;
 };
 
 export default useWindowWidth;
