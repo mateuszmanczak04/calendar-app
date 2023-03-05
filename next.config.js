33a5323d5d74bf49303e3dce66189dd5741794af
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-03-05 20:11:34 +0100

    remove env variables from next config

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { appDir: true },
-  env: {
-    MONGO_URI:
-      'mongodb+srv://Mateusz:NGDxHuFvInb67G8N@cluster0.mr0mbnp.mongodb.net/calendar-app?retryWrites=true&w=majority',
-    NEXTAUTH_SECRET: 'ZtFmg7tF2mbvhLj',
-  },
 };
 
 module.exports = nextConfig;
